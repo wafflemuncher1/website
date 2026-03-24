169cf113a972ea67f08d67baf50c2a2b82d745e9
zane <zanerisinger@gmail.com>
2026-03-24 09:46:13 -0400

    Your descriptive message

--- src/components/StickyHeader.tsx
+++ src/components/StickyHeader.tsx
@@ -37,7 +37,7 @@ const StickyHeader = () => {
           <img
   src={logo}
   alt="Glossworks"
-  className="h-[44px] md:h-[56px] w-auto"
+  className="h-[50px] md:h-[60px] w-auto"
   loading="eager"
 />
           
